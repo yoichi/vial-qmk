a69a2e1f088115711701a9cad0a4c43723961e1f
Yoichi NAKAYAMA <yoichi.nakayama@gmail.com>
2025-04-19 07:18:08 +0900

    Add meishi2:vial

---
+++ keyboards/biacco42/meishi2/keymaps/vial/config.h
@@ -0,0 +1,6 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+#define VIAL_KEYBOARD_UID {0x75, 0xFD, 0x07, 0xD3, 0xEC, 0x65, 0x64, 0x83}
+#define VIAL_UNLOCK_COMBO_ROWS {0, 1}
+#define VIAL_UNLOCK_COMBO_COLS {0, 1}
