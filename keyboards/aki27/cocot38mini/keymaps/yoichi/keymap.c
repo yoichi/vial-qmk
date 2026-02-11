aafe110d89f11e6bb0f6c7629598b0c353c8fa09
Yoichi NAKAYAMA <yoichi.nakayama@gmail.com>
2026-02-11 11:25:24 +0900

    Reassign control keys to avoid false hold detection

--- keyboards/aki27/cocot38mini/keymaps/yoichi/keymap.c
+++ keyboards/aki27/cocot38mini/keymaps/yoichi/keymap.c
@@ -45,8 +45,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_BASE] = LAYOUT(
         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,             KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
-        KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_TAB,  KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
-LSFT_T(KC_Z),    KC_X,    KC_C,    KC_V,LCTL_T(KC_B),     RCTL_T(KC_N),KC_M,    KC_COMM, KC_DOT,  RSFT_T(KC_SLSH),
+LCTL_T(KC_A),    KC_S,    KC_D,    KC_F,    KC_G,    KC_TAB,  KC_H,    KC_J,    KC_K,    KC_L,    RCTL_T(KC_SCLN),
+LSFT_T(KC_Z),    KC_X,    KC_C,    KC_V,    KC_B,             KC_N,    KC_M,    KC_COMM, KC_DOT,  RSFT_T(KC_SLSH),
               KC_LALT, KC_LGUI, LT(_NUMBER,KC_SPC),  MS_BTN1, KC_BSPC, LT(_SYMBOL,KC_ENT), LT(_FUNC,KC_DEL)
     ),
     [_NUMBER] = LAYOUT(
