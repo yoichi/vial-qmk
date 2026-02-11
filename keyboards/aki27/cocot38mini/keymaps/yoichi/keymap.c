f5398beb735c62ac3190ea4fef4ad9f8987b9de9
Yoichi NAKAYAMA <yoichi.nakayama@gmail.com>
2026-02-11 10:56:36 +0900

    Swap ESC and TAB

--- keyboards/aki27/cocot38mini/keymaps/yoichi/keymap.c
+++ keyboards/aki27/cocot38mini/keymaps/yoichi/keymap.c
@@ -45,13 +45,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_BASE] = LAYOUT(
         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,             KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
-        KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_ESC,  KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
+        KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_TAB,  KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
 LSFT_T(KC_Z),    KC_X,    KC_C,    KC_V,    KC_B,             KC_N,    KC_M,    KC_COMM, KC_DOT,  RSFT_T(KC_SLSH),
               KC_LALT, KC_LGUI, LT(_NUMBER,KC_SPC),  MS_BTN1, RCTL_T(KC_BSPC), LT(_SYMBOL,KC_ENT), LT(_FUNC,KC_DEL)
     ),
     [_NUMBER] = LAYOUT(
         KC_1,    KC_2,    KC_3,    KC_4,    KC_5,             KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
-        KC_INT1, KC_INT3, KC_GRV,  KC_LBRC, KC_RBRC, KC_TAB,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_INS,
+        KC_INT1, KC_INT3, KC_GRV,  KC_LBRC, KC_RBRC, KC_ESC,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_INS,
 LSFT_T(KC_CAPS),S(KC_HOME),S(KC_END),KC_QUOT,KC_BSLS,         KC_MINS, KC_EQL,  KC_COMM, KC_DOT,  LSFT_T(KC_SLSH),
                           KC_HOME, KC_END, _______,  MS_BTN2, IME_TGL, TT(_MEDIA), RCTL(KC_RALT)
     ),
