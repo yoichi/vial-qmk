d899a05ccc3307700f477c744f3530753238f77d
Yoichi NAKAYAMA <yoichi.nakayama@gmail.com>
2026-02-20 22:22:35 +0900

    KC_TAB -> LSFT_T(KC_TAB)

--- keyboards/aki27/cocot38mini/keymaps/yoichi/keymap.c
+++ keyboards/aki27/cocot38mini/keymaps/yoichi/keymap.c
@@ -45,7 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_BASE] = LAYOUT(
         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,             KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
-        KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_TAB,  KC_H,    KC_J,    KC_K,    KC_L,    RCTL_T(KC_MINS),
+        KC_A,    KC_S,    KC_D,    KC_F,  KC_G, LSFT_T(KC_TAB), KC_H,  KC_J,    KC_K,    KC_L,    RCTL_T(KC_MINS),
         KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,             KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_RSFT,
         KC_LALT, LCTL_T(KC_DEL), LT(_NUMBER,KC_SPC), MS_BTN1, KC_BSPC, LT(_SYMBOL,KC_ENT), KC_LGUI
     ),
