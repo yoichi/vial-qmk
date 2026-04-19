6a12a188a2ac319495d84eb2acd4a03cc37c8a44
Yoichi NAKAYAMA <yoichi.nakayama@gmail.com>
2026-04-19 22:26:54 +0900

    assign C(KC_SPACE) for set-mark-command on Emacs by two keys
    * Since CTL and SPC is assigned on the same key in _BASE layer,
    we cannot invoke their combination.
    * We can also use C-@ for set-mark-commnad on Emacs, but it requires
    pressing at least three keys.

--- keyboards/ymkn/neige/keymaps/yoichi/keymap.c
+++ keyboards/ymkn/neige/keymaps/yoichi/keymap.c
@@ -20,7 +20,7 @@ KC_LSFT,ALT_T(KC_Z),GUI_T(KC_X),LT(_NUMBER,KC_C), KC_V, KC_B, KC_N, LT(_SYMBOL, 
     [_NUMBER] = LAYOUT(
          KC_ESC,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_DEL,
         KC_QUOT, KC_MINS,  KC_EQL,  KC_GRV, KC_LBRC, KC_RBRC, KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, KC_SCLN, KC_BSLS,
-        _______, KC_INT1, KC_INT3, _______, KC_HOME,  KC_END, _______,TG(_FUNC),KC_COMM,  KC_DOT, KC_SLSH, _______
+        _______, KC_INT1, KC_INT3, _______, KC_HOME,  KC_END,C(KC_SPC),TG(_FUNC),KC_COMM, KC_DOT, KC_SLSH, _______
     ),
     [_SYMBOL] = LAYOUT(
       S(KC_INS), S(KC_1), S(KC_2), S(KC_3), S(KC_4), S(KC_5), S(KC_6), S(KC_7), S(KC_8), S(KC_9), S(KC_0), KC_CAPS,
