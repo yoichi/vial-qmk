e50dc1bfe482522bc867261194d0780b73c46fc1
Yoichi NAKAYAMA <yoichi.nakayama@gmail.com>
2026-04-19 11:48:45 +0900

    move mouse button

--- keyboards/aki27/cocot38mini/keymaps/yoichi/keymap.c
+++ keyboards/aki27/cocot38mini/keymaps/yoichi/keymap.c
@@ -47,19 +47,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,             KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
         KC_A,    KC_S,    KC_D,    KC_F,  KC_G, RCTL_T(KC_TAB), KC_H,  KC_J,    KC_K,    KC_L,    RCTL_T(KC_MINS),
         KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,             KC_N,    KC_M,    KC_COMM, KC_DOT,  RSFT_T(KC_SLSH),
-        KC_LALT, RCTL_T(KC_DEL), LT(_NUMBER,KC_SPC), MS_BTN1, KC_BSPC, LT(_SYMBOL,KC_ENT), KC_LGUI
+        KC_LALT, RCTL_T(KC_DEL), LT(_NUMBER,KC_SPC), KC_LGUI, MS_BTN1, LT(_SYMBOL,KC_ENT), KC_BSPC
     ),
     [_NUMBER] = LAYOUT(
         KC_1,    KC_2,    KC_3,    KC_4,    KC_5,             KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
         KC_MINS, KC_EQL,  KC_GRV,  KC_LBRC, KC_RBRC, KC_ESC,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_SCLN,
         KC_RSFT, XXXXXXX, KC_INS,  KC_HOME, KC_BSLS,          KC_QUOT, KC_END,  KC_COMM, KC_DOT,  RSFT_T(KC_SLSH),
-                          _______, _______, _______, MS_BTN2, _______, TT(_FUNC), IME_TGL
+                          _______, _______, _______, IME_TGL, MS_BTN2, TT(_FUNC), _______
     ),
     [_SYMBOL] = LAYOUT(
         S(KC_1), S(KC_2), S(KC_3), S(KC_4), S(KC_5),          S(KC_6), S(KC_7), S(KC_8), S(KC_9), S(KC_0),
 S(KC_MINS),S(KC_EQL),S(KC_GRV),S(KC_LBRC),S(KC_RBRC),S(KC_TAB),S(KC_LEFT),S(KC_DOWN),S(KC_UP),S(KC_RGHT),S(KC_SCLN),
         XXXXXXX, XXXXXXX,S(KC_INS),S(KC_HOME),S(KC_BSLS),     S(KC_QUOT),S(KC_END),S(KC_COMM),S(KC_DOT),S(KC_SLSH),
-                       _______, _______, TT(_MEDIA), MS_BTN2, XXXXXXX, XXXXXXX, _______
+                       _______, _______, TT(_MEDIA), _______, MS_BTN2, XXXXXXX, XXXXXXX
     ),
     [_FUNC] = LAYOUT(
         G(KC_1), G(KC_2), G(KC_3), G(KC_4), G(KC_5),          G(KC_6), G(KC_7), G(KC_8), G(KC_9), G(KC_0),
