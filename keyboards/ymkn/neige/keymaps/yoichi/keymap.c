4b5287fbcf9d104970b8a088417bb009f8f73507
Yoichi NAKAYAMA <yoichi.nakayama@gmail.com>
2026-04-24 19:28:00 +0900

    Move mod-tap setting from Z to Q

--- keyboards/ymkn/neige/keymaps/yoichi/keymap.c
+++ keyboards/ymkn/neige/keymaps/yoichi/keymap.c
@@ -13,9 +13,9 @@ enum layer_number {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT(
-         KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_BSPC,
+        KC_TAB, ALT_T(KC_Q), KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_BSPC,
   CTL_T(KC_SPC),    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L, KC_MINS,  KC_ENT,
-KC_LSFT,ALT_T(KC_Z),GUI_T(KC_X),LT(_NUMBER,KC_C), KC_V, KC_B, KC_N, LT(_SYMBOL, KC_M), KC_COMM, KC_DOT, KC_SLSH, KC_RSFT
+    KC_LSFT, KC_Z, GUI_T(KC_X), LT(_NUMBER,KC_C), KC_V, KC_B, KC_N, LT(_SYMBOL, KC_M), KC_COMM, KC_DOT, KC_SLSH, KC_RSFT
     ),
     [_NUMBER] = LAYOUT(
          KC_ESC,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_DEL,
@@ -29,8 +29,8 @@ _______,S(KC_INT1),S(KC_INT3),MO(_MEDIA),S(KC_HOME),S(KC_END), _______, _______,
     ),
     [_FUNC] = LAYOUT(
     MO(_CONFIG),   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,
-        _______, _______, _______, _______, _______, _______, MS_LEFT, MS_DOWN, MS_UP, MS_RGHT, _______, _______,
-      TG(_FUNC), _______, _______, _______, _______, _______, MS_BTN1, _______, MS_BTN2, _______, _______, TG(_FUNC)
+        _______, _______, _______, _______, _______, MS_WHLL, MS_LEFT, MS_DOWN,   MS_UP, MS_RGHT, MS_WHLR, _______,
+      TG(_FUNC), _______, _______, _______, _______, _______, MS_BTN1, _______, MS_BTN2, MS_WHLD, MS_WHLU, TG(_FUNC)
     ),
     [_MEDIA] = LAYOUT(
         _______, G(KC_1), G(KC_2), G(KC_3), G(KC_4), G(KC_5), G(KC_6), G(KC_7), G(KC_8), G(KC_9), KC_BRIU, KC_VOLU,
@@ -438,7 +438,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             }
             break;
 #else
-        case ALT_T(KC_Z):
+        case ALT_T(KC_Q):
             if (host_os == OS_WINDOWS) {
                 if (!record->tap.count) { // hold
                     if (record->event.pressed) {
@@ -450,7 +450,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 }
             }
             break;
-        case GUI_T(KC_X):
+    case GUI_T(KC_X):
             if (host_os == OS_WINDOWS) {
                 if (!record->tap.count) { // hold
                     if (record->event.pressed) {
