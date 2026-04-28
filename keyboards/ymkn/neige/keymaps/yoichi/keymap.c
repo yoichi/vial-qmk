98405c56c119c56130faba7973e9bd516b008143
Yoichi NAKAYAMA <yoichi.nakayama@gmail.com>
2026-04-29 07:03:55 +0900

    Add Mod-Tap on the upper row

--- keyboards/ymkn/neige/keymaps/yoichi/keymap.c
+++ keyboards/ymkn/neige/keymaps/yoichi/keymap.c
@@ -13,7 +13,7 @@ enum layer_number {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT(
-        KC_TAB, ALT_T(KC_Q), KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_BSPC,
+  LT(_SYMBOL,KC_TAB), ALT_T(KC_Q), KC_W,  KC_E,  KC_R,  KC_T,  KC_Y,  KC_U,  KC_I,  KC_O,  KC_P, LT(_NUMBER,KC_BSPC),
   CTL_T(KC_SPC),    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L, KC_MINS,  KC_ENT,
     KC_LSFT, KC_Z, GUI_T(KC_X), LT(_NUMBER,KC_C), KC_V, KC_B, KC_N, LT(_SYMBOL, KC_M), KC_COMM, KC_DOT, KC_SLSH, KC_RSFT
     ),
